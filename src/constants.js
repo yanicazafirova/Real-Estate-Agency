22c7eaea1a6826ec0fe6d3a90b14d1973af10943
Yanica Zafirova <yanica_zafirova@abv.bg>
2023-02-11 18:45:27 +0200

    Add constants

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+exports.PORT = 5000;
+exports.CONNECTION_STRING = 'mongodb://127.0.0.1:27017/artGallery';
+exports.JWT_SECRET = 'oeklasmfrigvfdv[perinmkalnrwenfvjnklrg/';
+exports.AUTH_COOKI_NAME = 'auth_jwt';
+exports.SALT_ROUNDS = 10;
