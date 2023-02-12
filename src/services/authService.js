37dd63219b2c8ae6de984f0fe2a7e441cf5f1429
Yanica Zafirova <yanica_zafirova@abv.bg>
2023-02-12 12:55:25 +0200

    Add authentication service

---
+++ src/services/authService.js
@@ -0,0 +1,28 @@
+const User = require('../models/User');
+const jwt = require('../utils/jwt');
+const { JWT_SECRET } = require('../constants');
+
+exports.register = (userData) => User.create(userData);
+
+exports.login = async (username, password) => {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        throw new Error('Email or password dont match!');
+    }
+
+    const isValid = await user.validatePassword(password);
+
+    if (!isValid) {
+        throw new Error('Email or password dont match!');
+    }
+    
+    const payload = {
+        _id: user._id,
+        username: user.username,
+    };
+
+    const token = await jwt.sign(payload, JWT_SECRET, {expiresIn: '2h'});
+    
+    return token;
+};
